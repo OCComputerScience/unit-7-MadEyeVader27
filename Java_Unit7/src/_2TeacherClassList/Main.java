6085c472c80fb0261822b18e3af9e8f4665c6dc7
MadEyeVader27 <KaneWP@oldcolony.info>
2024-12-18 11:01:22 -0500

    All of Unit 7

---
+++ Java_Unit7/src/_2TeacherClassList/Main.java
@@ -0,0 +1,16 @@
+package _2TeacherClassList;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Student student1 = new Student("William Kane", 11);
+        Student student2 = new Student("Audrey Perkins", 11);
+        Student student3 = new Student("Kaysen Hill", 11);
+        Student student4 = new Student("Sean Plouffe", 11);
+        Student student5 = new Student("Maxx Boutin", 11);
+
+        Student.printClassList();
+    }
+
+}
